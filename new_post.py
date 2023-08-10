b3b9537adaddb1a7b686fa85dd78d7d269122768
Welzagen <57945574+Welzagen@users.noreply.github.com>
2023-08-10 21:49:45 +0300

    new_post.py

--- new_post.py
+++ new_post.py
@@ -1,5 +1,5 @@
 import time
-
+import random
 import instaloader
 from pyrogram import Client
 
@@ -51,4 +51,4 @@ with Client("my_acc", API_ID, API_HASH) as app:
             send_message(last_post)
             posts_.append(last_post)
 
-        time.sleep(8000)
+        time.sleep(randit(6000,8000))
